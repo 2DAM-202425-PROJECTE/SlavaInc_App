b96b89ce0b4af04f3d69575975bd59e0edb038ee
omihalcea <omihalcea@iesebre.com>
2025-05-20 00:01:57 +0200

    Vercel

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-# Instal·la dependències
 npm install
-
-# Construeix els assets
 npm run build
+mkdir -p public/build
+cp -R resources/js public/build/
+cp -R resources/css public/build/
+cp public/index.html public/build/
